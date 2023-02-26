073c2366c5c4c212d621d486ac389cc15a4f28b1
franciscocoya <kikocoya@gmail.com>
2023-02-27 00:38:54 +0100

    enh: Configuración inicial del fichero server.ts de la API Rest

--- restapi/server.ts
+++ restapi/server.ts
@@ -1,37 +1,29 @@
 import express, {
   Application,
-  Request,
   RequestHandler,
-  Response,
 } from "express";
-import cors from "cors";
+import corsConfiguration from './config/cors'
 import morgan from "morgan";
-import bp from "body-parser";
 import promBundle from "express-prom-bundle";
 import api from "./api";
+import { API_PORT } from "./config/dotenv";
 
 import pointRouter from "./routes/point.route";
 
 const app: Application = express();
-const port: number = 5000;
+const port: number = API_PORT;
 
 const metricsMiddleware: RequestHandler = promBundle({ includeMethod: true });
 app.use(metricsMiddleware);
 
-app.use(bp.json());
+app.use(express.json());
 
 app.use(morgan("combined"));
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(corsConfiguration);
 
-const allowedOrigins = ["*"];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
-
-app.use(cors(options));
+// Rutas
 app.use("/api", api);
-
 app.use("/point", pointRouter);
 
 app
