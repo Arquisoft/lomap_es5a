0d7f89b18ce8751ffbe9cecc0edfc05279e20057
UO282337 <UO282337@uniovi.es>
2023-03-01 20:22:44 +0100

    Solucionado el problema con los arrays, pararemos de usar un directorio model para los tipos

---
+++ restapi/model/grupoModel.ts
@@ -0,0 +1,42 @@
+import { Schema, Document, Model, model } from 'mongoose';
+
+interface IPersona extends Document {
+    id: string;
+    role: string;
+    verified: boolean;
+}
+
+interface IGrupo extends Document {
+    id: string; 
+    personas: IPersona[];
+}
+
+const personaSchema = new Schema<IPersona>({
+    id: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    verified: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const grupoSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    personas: {
+        type: [personaSchema],
+        required: true
+    }
+});
+
+const GrupoModel: Model<IGrupo> = model<IGrupo>('Grupo', grupoSchema);
+
+export default GrupoModel;
