298b1c0e40c36cb174f948e508a65a3417427e3f
franciscocoya <kikocoya@gmail.com>
2023-03-04 17:11:46 +0100

    feat: Barra de filtros para categorias de puntos de interes
    Implementación del componente de la UI en Home

---
+++ webapp/src/components/filters/SingleFilter.tsx
@@ -0,0 +1,24 @@
+import "../../public/css/components/filters/SingleFilter.scss";
+import Icon from '@mui/material/Icon';
+
+type Props = {
+  iconFilename: any;
+  iconAlt?: string;
+  text: string;
+  isActive?: boolean;
+};
+
+function SingleFilter({ iconFilename, iconAlt, text, isActive=false }: Props) {
+  return (
+    <div className={`single-filter-container ${isActive && "category-filter--active"}`}>
+      <Icon>
+        {
+          iconFilename
+        }
+      </Icon>
+      <span>{text}</span>
+    </div>
+  );
+}
+
+export default SingleFilter;
