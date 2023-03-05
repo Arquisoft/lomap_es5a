78208690d13b681a922c46da95c8ff09ffc0d645
franciscocoya <kikocoya@gmail.com>
2023-03-06 00:30:24 +0100

    enh: Creación de la primera versión del componente Popup #63 para el mapa
    Siguiendo prototipo especificado

--- webapp/src/components/maps/popups/BaseMapPopup.tsx
+++ webapp/src/components/maps/popups/BaseMapPopup.tsx
@@ -1,36 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseBadge from "src/components/badges/BaseBadge";
 import ProfileInfoWithFollowButton from "src/components/profiles/ProfileInfoWithFollowButton";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ShareIcon from '@mui/icons-material/ShareOutlined';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShareIcon from "@mui/icons-material/ShareOutlined";
 
 import "../../../public/css/components/maps/popups/BasePopup.scss";
+import BaseButton from "src/components/buttons/BaseButton";
 
 function BaseMapPopup({ ...props }) {
+  const [showCategoryBadge, setShowCategoryBadge] = useState(false);
 
   const badgeStyles = {
     backgroundColor: "white",
     position: "absolute",
     top: "10px",
     right: "10px",
-  }
+  };
+
+  const handleShowBadge = (show: boolean) => {
+    setShowCategoryBadge(show);
+  };
 
   return (
     <div className="base-popup-modal">
-      <div className="base-popup-modal__head">
-        <BaseBadge text={"RESTAURANTE"} styles={badgeStyles}/>
-        <img src="https://images.unsplash.com/photo-1640276380950-9fad7f2aba89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80" alt="" />
+      <div
+        className="base-popup-modal__head"
+        onMouseEnter={() => handleShowBadge(true)}
+        onMouseLeave={() => handleShowBadge(false)}
+      >
+        {showCategoryBadge && (
+          <BaseBadge text={"RESTAURANTE"} styles={badgeStyles} />
+        )}
+        <img
+          src="https://images.unsplash.com/photo-1640276380950-9fad7f2aba89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+          alt=""
+        />
       </div>
       <div className="base-popup-modal__body">
-          <ProfileInfoWithFollowButton />
-          <div className="popup-modal-social-icons">
-            <ShareIcon sx={{
-              color: "#2B3467"
-            }}/>
-            <FavoriteBorderIcon sx={{
-              color: "#ef233c"
-            }}/>
-          </div>
+        <ProfileInfoWithFollowButton />
+        <div className="popup-modal-social-icons">
+          <ShareIcon
+            sx={{
+              color: "#2B3467",
+            }}
+          />
+          <FavoriteBorderIcon
+            sx={{
+              color: "#ef233c",
+            }}
+          />
+        </div>
+      </div>
+      <div className="base-popup-modal__footer">
+        <div className="popup-model-footer__contact-info">
+          <p>Sidrería Tierra Astur</p>
+          <p>Calle Gascona, 1</p>
+        </div>
+        <BaseButton type="button-blue" text="Ver punto" />
       </div>
     </div>
   );
