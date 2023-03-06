74c756896cebfb681c7bed64660b0604175de974
RichardPix12 <richard.marques1234@gmail.com>
2023-03-06 12:26:01 +0100

    estilo y feat parte 1 about

--- webapp/src/pages/about/AboutPage.tsx
+++ webapp/src/pages/about/AboutPage.tsx
@@ -1,8 +1,27 @@
+
+import AuthenticatedLayout from "src/layouts/AutenticatedLayout";
+
 function AboutPage() {
   return (
-    <>
-      <p>Sobre nosotros</p>
-    </>
+    <div>    
+      <AuthenticatedLayout>
+        <div className="about-title">
+          Acerca de
+        </div>
+
+        <div className="about-slogan">
+          Descubre nuevos lugares, comparte y más
+        </div>
+
+        <div className="about-image">
+
+        </div>
+
+        <div className="about-info">
+
+        </div>
+      </AuthenticatedLayout>
+    </div>
   );
 }
 
