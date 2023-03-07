8187a781878364983060d82ec93d51853d1dd02c
RichardPix12 <richard.marques1234@gmail.com>
2023-03-07 18:02:03 +0100

    empezar a trabajar la info aplicacion

--- webapp/src/components/about/PageInfo.tsx
+++ webapp/src/components/about/PageInfo.tsx
@@ -1,7 +1,18 @@
+import { InfoBox } from "./info/InfoBox"
+import "../../public/css/components/about/PageInfo.css"
+
 function PageInfo(){
     return(
-        <div>
-            Pageinfo
+        <div className="info-principal">
+            <div className="info-comp1">
+                <InfoBox />
+            </div>
+            <div className="info-comp2">
+                <InfoBox />
+            </div>
+            <div className="info-comp3">
+                <InfoBox />
+            </div>
         </div>
     )
 }
---
+++ webapp/src/components/about/info/InfoBox.tsx
@@ -0,0 +1,20 @@
+import "../../../public/css/components/about/info/InfoBox.css"
+
+function InfoBox(){
+    return(
+        <div className="infobox-main">
+            <div className="infobox-image">
+                
+            </div>
+            <div className="infobox-title">
+                Privacidad
+            </div>
+            <div className="infobox-description">
+                Tu decides que puntos compartir. La información se almacena de forma distribuida.
+            </div>
+        </div>
+    )
+}
+
+
+export {InfoBox}
