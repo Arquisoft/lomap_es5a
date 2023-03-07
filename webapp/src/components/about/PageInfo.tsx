e376e39fae9d5c3e53a262b2d67247c63874175a
franciscocoya <kikocoya@gmail.com>
2023-03-07 23:05:30 +0100

    fix: Incluyendo iconos MUA en componente PageInfo
    Modificación en componente InfoBox, adaptando a TS y añadiendo propiedad icono

--- webapp/src/components/about/PageInfo.tsx
+++ webapp/src/components/about/PageInfo.tsx
@@ -1,26 +1,31 @@
 import { InfoBox } from "./info/InfoBox"
 import "../../public/css/components/about/PageInfo.css"
 
+
+import FingerprintRoundedIcon from '@mui/icons-material/FingerprintRounded';
+import ShareRoundedIcon from '@mui/icons-material/ShareRounded';
+import GppGoodRoundedIcon from '@mui/icons-material/GppGoodRounded';
+
 function PageInfo(){
     return(
         <div className="info-principal">
             <div className="info-comp1">
                 <InfoBox 
-                    image="/src/public/images/icons/about/privacidad.png"
+                    icon={<FingerprintRoundedIcon fontSize="large"/>}
                     title="Privacidad" 
                     description="Tu decides que puntos compartir. La información se almacena de forma distribuida."
                  />
             </div>
             <div className="info-comp2">
                 <InfoBox 
-                    image=""
+                    icon={<ShareRoundedIcon fontSize="large"/>}
                     title="Visibilidad" 
                     description="Si eres un negocio local, compártelo con tus amigo y el resto de usuarios." 
                 />
             </div>
             <div className="info-comp3">
-                <InfoBox 
-                    image=""
+                <InfoBox
+                    icon={<GppGoodRoundedIcon fontSize="large"/>}
                     title="Seguridad" 
                     description="Tu eres el propietario de tus publicaciones, no almacenamos tus publicaciones." 
                 />
