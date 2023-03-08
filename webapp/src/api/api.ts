d116d1e56c5a369d8c9a4f2260a6c5379ea9c224
UO282337 <UO282337@uniovi.es>
2023-03-08 19:39:14 +0100

    Actualizada la api de conexión del backEnd y frontEnd

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,7 +1,9 @@
 import {User} from '../shared/shareddtypes';
+import { IPoint } from "../../../restapi/models/point.model";
+import { IOpinion } from '../../../restapi/models/opinion.model';
 
 export async function addUser(user:User):Promise<boolean>{
-    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
+    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5001/api'
     let response = await fetch(apiEndPoint+'/users/add', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
@@ -14,8 +16,40 @@ export async function addUser(user:User):Promise<boolean>{
 }
 
 export async function getUsers():Promise<User[]>{
-    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
+    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5001/api'
     let response = await fetch(apiEndPoint+'/users/list');
     //The objects returned by the api are directly convertible to User objects
     return response.json()
 }
+
+export async function addPoint(point: IPoint) {
+  const apiEndPoint = process.env.REACT_APP_API_URI;
+
+  let response = await fetch(apiEndPoint + '/add', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'auth-token': localStorage.getItem('token') as string },
+    body: JSON.stringify(point)
+  });
+  return response.status === 201;
+}
+
+export async function deletePoint(idPoint: string): Promise<boolean> {
+  const apiEndPoint = process.env.REACT_APP_API_URI;
+  let response = await fetch(apiEndPoint + '/delete/' + idPoint, {
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json', 'auth-token': localStorage.getItem('token') as string},
+
+  });
+  return response.status === 200;
+}
+
+export async function reviewPoint(idPoint: string, opinion: IOpinion) {
+  const apiEndPoint = process.env.REACT_APP_API_URI;
+  let response = await fetch(apiEndPoint + '/review/' + idPoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(opinion)
+  });
+  return response.status === 200;
+}
+
