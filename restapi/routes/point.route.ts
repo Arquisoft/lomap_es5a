99aee852f8d82cff98ee2d1a0e1f2b9d5ca0e8cb
UO282337 <UO282337@uniovi.es>
2023-03-08 11:28:35 +0100

    Realizados los cambios con la rama de Marcos de BackEnd

--- restapi/routes/point.route.ts
+++ restapi/routes/point.route.ts
@@ -5,15 +5,10 @@ import { findAll } from "../controllers/point.controller";
 const router: Router = express.Router();
 
 router.get("/all/:webId", findAll);
-
-<<<<<<< HEAD
 router.post("/api/maps/point/add", addPoint);
 router.post("/api/maps/point/delete/:pointId", addPoint);
 router.post("/api/maps/point/review/:pointId", addPoint);
-=======
-//router.post("/api/maps/add", addPoint);
-// deletePointById
-// reviewPointByUser
->>>>>>> origin/MarcosBackEnd
+
+
 
 export default router;
