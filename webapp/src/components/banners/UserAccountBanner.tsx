2fb8282d102221151afbfa908696d4edd2a4a5b5
franciscocoya <kikocoya@gmail.com>
2023-03-10 00:19:48 +0100

    style: Fondo por defecto para el banner de la cuenta de usuario
    Si no se especifica fondo, color solido por defecto

--- webapp/src/components/banners/UserAccountBanner.tsx
+++ webapp/src/components/banners/UserAccountBanner.tsx
@@ -1,25 +1,37 @@
-
-import "../../public/css/components/banners/AccountBanner.scss";
 import BaseProfileItem from "../profiles/BaseProfileItem";
+import "../../public/css/components/banners/AccountBanner.scss";
 
+/**
+ * @param imgBackground: Si no se especifica, se muestra un fondo por defecto.
+ */
 type Props = {
-  _className: string,
-  imgBackground: string
-}
+  _className: string;
+  imgBackground?: string;
+};
 
 //TODO: {_className}: ComponentClassName,
 
-function UserAccountBanner(
-  {_className, imgBackground}: Props
-) {
+function UserAccountBanner({ _className, imgBackground }: Props) {
   return (
     <div className={`user-account-banner-container ${_className}`}>
-      <div className="user-account-banner__background" ><img src={imgBackground} alt="" /></div>
+      <div
+        className="user-account-banner__background"
+      >
+        {
+          imgBackground &&
+          <img src={imgBackground} alt="" />
+        }
+      </div>
       <div className="user-account-banner__overlay"></div>
       {/* Imagen de fondo como prop */}
       {/* Componente para mostrar avatar y nombre del usuario */}
-      <BaseProfileItem />
-      Banner de la cuent
+      <div className="user-account-profile-item__wrapper">
+        <BaseProfileItem
+          profileImage="https://randomuser.me/api/portraits/women/10.jpg"
+          name="Sofía Rodríguez"
+          email="sofiarodriguez@uniovi.es"
+        />
+      </div>
     </div>
   );
 }
