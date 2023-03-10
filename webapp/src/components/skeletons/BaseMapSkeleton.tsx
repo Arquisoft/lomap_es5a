5dd22775a0d39f196421226ddd6e854d04443cf9
franciscocoya <kikocoya@gmail.com>
2023-03-10 12:16:25 +0100

    feat: Skeleton de carga para componente BaseMap

---
+++ webapp/src/components/skeletons/BaseMapSkeleton.tsx
@@ -0,0 +1,18 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import "../../public/css/components/skeletons/BaseMapSkeleton.scss";
+
+function BaseMapSkeleton() {
+  return (
+    <div className="basemap-skeleon-container">
+      <Skeleton
+        width="100%"
+        height="100%"
+        style={{ borderRadius: 10 }}
+        duration={10}
+      />
+    </div>
+  );
+}
+
+export default BaseMapSkeleton;
