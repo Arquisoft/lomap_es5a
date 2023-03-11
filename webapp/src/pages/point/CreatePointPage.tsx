31edc2e80860ae439aac2c887512fec28abae914
RichardPix12 <richard.marques1234@gmail.com>
2023-03-11 10:57:57 +0100

    Incorporar el menu

--- webapp/src/pages/point/CreatePointPage.tsx
+++ webapp/src/pages/point/CreatePointPage.tsx
@@ -1,14 +1,24 @@
 import AuthenticatedLayout from "src/layouts/AutenticatedLayout"
 import "../../public/css/pages/points/CreatePointPage.css"
+import TopAccountAside from "src/components/asides/accountAside/TopAccountAside"
 
 function CreatePointPage(){
-    return(
-        <AuthenticatedLayout>
-            <div>
-                Crear Puntos
+   
+return (
+    <AuthenticatedLayout>
+        <div className='main-create-points'>
+            <div className='left-aside-create-points'>
+              <TopAccountAside />
             </div>
-        </AuthenticatedLayout>
-    )
+            <div className='formulario-create-points'>
+              Formulario
+            </div>
+            <div className='marcador-create-points'>
+              mapa
+            </div>
+        </div>
+    </AuthenticatedLayout>
+  )
 }
 
 export  default CreatePointPage
