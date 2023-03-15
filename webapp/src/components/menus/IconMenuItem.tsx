fb3ca0c36f5b2c1d87b1ea7f810d7f2f589ec02b
franciscocoya <kikocoya@gmail.com>
2023-03-16 00:52:00 +0100

    enh: add url and link to account menu items

--- webapp/src/components/menus/IconMenuItem.tsx
+++ webapp/src/components/menus/IconMenuItem.tsx
@@ -1,6 +1,5 @@
-import "../../public/css/components/menus/menuItems/IconMenuItem.scss";
 import Icon from "@mui/material/Icon";
-import { Link } from "react-router-dom";
+import "../../public/css/components/menus/menuItems/IconMenuItem.scss";
 
 /**
  * @param name: Nombre del elemento de menu.
@@ -13,8 +12,12 @@ type Props = {
 };
 
 function IconMenuItem({ name, iconName, url }: Props) {
+  const handleRedirect = () => {
+    window.location.href = url || "";
+  };
+
   return (
-    <li className="icon-menu-item">
+    <li className="icon-menu-item"onClick={handleRedirect}>
       <Icon sx={{ fontSize: 24 }}>{iconName}</Icon>
       <span>{name}</span>
     </li>
