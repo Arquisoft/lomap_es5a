1ead02ce7274132bca2ce56f954304f7087c5c04
UO271572 <uo271572@uniovi.es>
2023-04-04 22:12:32 +0200

    baseProfile y ProfileInfo tests unitarios

---
+++ webapp/src/components/profiles/ProfileInfoWithFollowButton.test.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {render,fireEvent, cleanup } from '@testing-library/react';
+import ProfileInfoWithFollowButton from "./ProfileInfoWithFollowButton";
+
+//mockeamos la consola pra realizar la prueba
+console.log = jest.fn();
+
+
+describe('ProfileInfoWithFollowButton componente', () => {
+
+    afterAll(cleanup);
+  
+   it('Renderiza', () => {
+      const { getByText, getByAltText, getByRole } = render(
+        <ProfileInfoWithFollowButton 
+          name='John Doe' 
+          imageUrl='https://example.com/image.jpg' 
+          webId='https://example.com/johndoe'
+        />
+      );
+      
+      const nameElement = getByText('John Doe');
+      const imageElement = getByAltText('John Doe');
+      const followButton = getByRole('button', {name: 'Seguir'});
+      
+      expect(nameElement).toBeInTheDocument();
+      expect(imageElement).toBeInTheDocument();
+      expect(followButton).toBeInTheDocument();
+    });
+  
+    it('Llamamos a la funcion', () => {
+      const { getByRole } = render(
+        <ProfileInfoWithFollowButton 
+          name='John Doe' 
+          imageUrl='https://example.com/image.jpg' 
+          webId='https://example.com/johndoe'
+        />
+      );
+  
+      const followButton = getByRole('button', {name: 'Seguir'});
+  
+      fireEvent.click(followButton);
+  
+      expect(console.log).toHaveBeenCalledWith('Followed', 'https://example.com/johndoe');
+    });
+  });
