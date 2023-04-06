4d3733e21453d7c8f5c6e781f1e32fadb108557a
UO271572 <uo271572@uniovi.es>
2023-04-06 15:39:56 +0200

    Configuracion babael para Javascript moderno

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -1,5 +1,10 @@
 export default {
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
-    }, 
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+        "^.+\\.tsx?$": "ts-jest",
+        
+    },
+    transformIgnorePatterns: [
+        "/node_modules/(?!react-leaflet)/"
+      ],
 }
