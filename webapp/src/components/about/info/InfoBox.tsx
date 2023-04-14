e46086a23df418f1bfd6f1657779815130e1d5c3
RichardPix12 <richard.marques1234@gmail.com>
2023-04-14 17:35:27 +0200

    Quitados test e2e que no estan implementados

--- webapp/src/components/about/info/InfoBox.tsx
+++ webapp/src/components/about/info/InfoBox.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 function InfoBox({ icon, title, description }: Props) {
   return (
-    <div className="infobox-main">
-      <div className="infobox-image" >{icon}</div>
+    <div className="infobox-main" role="contentinfo">
+      <div className="infobox-image" role="img">{icon}</div>
       <div className="infobox-title">{title}</div>
       <div className="infobox-description">{description}</div>
     </div>
