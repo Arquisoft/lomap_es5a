0b8e48aab80559ac4a61e5b81ab9c15421f516e2
UO271572 <uo271572@uniovi.es>
2023-04-19 19:11:49 +0200

    añadiendo funciones

--- webapp/src/components/asides/accountAside/BottomAccountAside.tsx
+++ webapp/src/components/asides/accountAside/BottomAccountAside.tsx
@@ -1,27 +1,72 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import "../../../public/css/components/asides/accountAside/BottomAccountAside.css";
 import { generateUUID } from "../../../utils/stringUtils";
+import BaseCheckboxInput from "../../inputs/BaseCheckboxInput";
+import { el } from "date-fns/locale";
+
+
 
 
 function BottomAccountAside(){
-    //cargar lsita de amigos
 
+    type Amigo = {
+        id: string;
+        nombre: string;
+        imagen: string;
+    }
+    const amigo:Amigo = {
+        id: "",
+        nombre: "",
+        imagen: ""
+    }
+
+    //cargar lista de amigos
+    
     //contiene la lista de amigos que podran recogerse fuera de este componente
+    const [amigos,setAmigos] = useState<Amigo[]>([]);
+    //Funciones para añadir o eliminar y comprobar la lista de amigos
+    const añadirAmigo = (amigo:Amigo) => {
+        setAmigos([...amigos,amigo]);
+    };
+    const eliminarAmigo = (amigoAEliminar:Amigo) => {
+        setAmigos(amigos.filter(amigo => amigo.id !== amigoAEliminar.id));
+    };
+    const verificaAmigo = (amigoVerificar:Amigo) => {
+        return amigos.some(amigo => amigo.id === amigoVerificar.id);
+    };
 
     //amigos seleccionados
+    const handleCheckbox = (isChecked:boolean) => {
+        //TODO: añadir a la lista de amigos si se selecciona o eliminarlo si no esta seleccionado
+        console.log(isChecked)
+        if(isChecked){
+            //añadirlo
+
+            //limpiar el valor de amigo
+            
+        }else{
+            //borrar
+
+            //limpiar el valor de amigo
+
+        }
+    };
 
     return(
         <div>
             <div className="base-textarea-container">
                 <label htmlFor={generateUUID()}>{"Compartido con"}</label>
                 <div className="friend-list">
-                    {friends.map((friend)=>{
+                    {/*{friends.map((friend)=>{
+                        //const amigo:Amigo = {} vamos creando el amigo
                         //añadir el amigo con sus datos en el cuadro
                         //imagen
                         //nombre
                         //checkbox //añadir una funcion para cuando este seleccionado se almacene en amigos seleccionados
-                        //<BaseCheckboxInput />
+                        //<BaseCheckboxInput ...añadir funcionescallback si estan pulsados y añadir el amigo en caso de q lo este.../>
                     })}
+                    */}
+                    <BaseCheckboxInput onCheckboxChange={handleCheckbox}/>
                 </div>
             </div>
         </div>
--- webapp/src/components/inputs/BaseCheckboxInput.tsx
+++ webapp/src/components/inputs/BaseCheckboxInput.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import {BaseCheckboxInputProps} from "../../shared/shareddtypes";
 
 
-function BaseCheckboxInput(){
+
+function BaseCheckboxInput({onCheckboxChange} : BaseCheckboxInputProps){
     const [isChecked, setIsChecked] = React.useState(false); // Estado para mantener el valor del checkbox
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked); // Manejador de eventos para cambiar el valor del estado del checkbox
+    //llamada callback al elemento padre, comprobar que la funcion esa definida si onCheckboxChange fuese ?
+    onCheckboxChange(!isChecked);
   };
 
     return(
