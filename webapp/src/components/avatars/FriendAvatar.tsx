a2b02b346705dfee058dca73a0c926add9f397ad
Marcos Valín Fernández <UO277414@uniovi.es>
2023-04-25 11:05:09 +0200

    Testing: test del componente FriendAvatar realizado

--- webapp/src/components/avatars/FriendAvatar.tsx
+++ webapp/src/components/avatars/FriendAvatar.tsx
@@ -9,10 +9,10 @@ type Props = {
 function FriendAvatar({name,imgUrl,webId}:Props){
 
     return(
-        <div className="friend-comp">
-            <img src={imgUrl}/>
-            <p>{name}</p>
-            <p>{webId}</p>
+        <div className="friend-comp" role="friend-comp">
+            <img src={imgUrl} role="friend-imagen"/>
+            <p role="friend-name">{name}</p>
+            <p role="friend-webid">{webId}</p>
         </div>
     );
 }
