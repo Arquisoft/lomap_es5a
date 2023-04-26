5e2abb7025a7c68261f77d9f77a470ebcb862069
UO271572 <uo271572@uniovi.es>
2023-04-26 18:32:16 +0200

    añadida funcionalidad de añadir amigo

--- webapp/src/components/asides/accountAside/BottomAccountAside.tsx
+++ webapp/src/components/asides/accountAside/BottomAccountAside.tsx
@@ -1,15 +1,18 @@
 import { useSession } from "@inrupt/solid-ui-react";
-import { useEffect } from "react";
-import { getAllFriends } from "../../../api/friends.api";
+import { useEffect, useState } from "react";
 import "../../../public/css/components/asides/accountAside/BottomAccountAside.css";
 import { useUserStore } from "../../../store/user.store";
 import { generateUUID } from "../../../utils/stringUtils";
 import FriendAvatar from "../../avatars/FriendAvatar";
+import BaseTextInput from "../../inputs/BaseTextInput";
+import BaseButton from "../../buttons/BaseButton";
+import {addFriend,getAllFriends} from "../../../api/friends.api";
 
 function BottomAccountAside(){
 
     const { session } = useSession();
     const {setFriends, friends} = useUserStore();
+    const [friendWebId, setFriendWebId] = useState("");
 
     //cargar lista de amigos
     const loadAllFriends = async () => {
@@ -19,12 +22,31 @@ function BottomAccountAside(){
     
     useEffect(() => {
         loadAllFriends();
-    }, []);
+    }, [friends]);
     
-    
+    const handleAddFriend = () => {
+        addFriend(session.info.webId as string, friendWebId);
+    };
 
     return(
         <div className="friends-container-aside">
+            <div className="top-acc-aside-title">Amigos</div>
+            <BaseTextInput
+                label={""} 
+                type={"text"} 
+                placeholder="ej: amigo.inrupt.net"
+                onChange={ (e) => 
+                    {setFriendWebId(e.target.value)}
+                }
+            />
+            <BaseButton
+                data-testid="create-point-button"
+                type="button-blue"
+                text="Agregar amigo"
+                onClick={handleAddFriend}
+            />
+
+
                 <label htmlFor={generateUUID()}></label>
                 <div className="friend-list-aside">
                     {friends ? 
