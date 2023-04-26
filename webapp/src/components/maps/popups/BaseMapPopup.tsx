11809fc2bef57ea84f20254aca775b96587fc05e
UO282337 <UO282337@uniovi.es>
2023-04-26 23:35:52 +0200

    fix: firebase and bugs

--- webapp/src/components/maps/popups/BaseMapPopup.tsx
+++ webapp/src/components/maps/popups/BaseMapPopup.tsx
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from "react";
+
 import { useNavigate } from "react-router-dom";
+
 import "../../../public/css/components/maps/popups/BasePopup.scss";
+
 import { BaseMapPopupProps } from "../../../shared/shareddtypes";
+
 import BaseBadge from "../../badges/BaseBadge";
+
 import BaseButton from "../../buttons/BaseButton";
+
 import ProfileInfoWithFollowButton from "../../profiles/ProfileInfoWithFollowButton";
+
 import { usePointDetailsStore } from "../../../store/point.store";
+
 import { canonizeUrl } from "../../../utils/stringUtils";
+
 import { availableCategories } from "../../../helpers/CategoryFilterHelper";
+
 import {
   isPointSaved,
   savePoint,
   unsavePoint,
 } from "../../../api/save.point.api";
+
 import { useSession } from "@inrupt/solid-ui-react";
+
 import {
   FavoriteIcon,
   FavoriteBorderIcon,
@@ -21,23 +33,34 @@ import {
 
 function BaseMapPopup({
   name,
+
   location,
+
   image,
+
   owner,
+
   category,
+
   point,
 }: BaseMapPopupProps) {
   const [showCategoryBadge, setShowCategoryBadge] = useState(false);
+
   const [isSaved, setIsSaved] = useState(false);
+
   const { setPointToShow } = usePointDetailsStore();
+
   const { session } = useSession();
 
   const navigate = useNavigate();
 
   const badgeStyles = {
     backgroundColor: "white",
+
     position: "absolute",
+
     top: "10px",
+
     right: "10px",
   };
 
@@ -46,32 +69,49 @@ function BaseMapPopup({
   };
 
   /**
+
    * Guardar el punto actual en los puntos guardados.
+
    */
+
   const handleSavePoint = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
+
     if (point) {
-      await savePoint(point, session);
+      await savePoint(point, session).then(() => {
+        setIsSaved(true);
+      });
     }
   };
 
   /**
+
    * Eliminar de puntos guardados el punto actual.
+
    */
+
   const handleUnSavePoint = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
+
     if (point) {
-      await unsavePoint(point?._id, session?.info.webId as string);
+      await unsavePoint(point?._id, session?.info.webId as string).then(() => {
+        setIsSaved(false);
+      });
     }
   };
 
   /**
+
    * Comprobar si el punto ha sido guardado por el usuario en sesion.
+
    * @returns
+
    */
+
   const checkIfPointIsSaved = async () => {
     if (!point) {
       setIsSaved(false);
+
       return;
     }
 
@@ -86,20 +126,25 @@ function BaseMapPopup({
     if (point) {
       setPointToShow(point);
     }
+
     if (point?.name) {
       navigate(canonizeUrl("/points", point.name));
     }
   };
 
   /**
+
    * Comprueba si el usuario en sesion es el propietario del punto.
+
    * En caso de serlo, no se le permite guardar el punto.
+
    * @returns
+
    */
 
   useEffect(() => {
     checkIfPointIsSaved();
-  }, [isSaved]);
+  }, []);
 
   return (
     <div className="base-popup-modal">
@@ -117,48 +162,53 @@ function BaseMapPopup({
             styles={badgeStyles as React.CSSProperties}
           />
         )}
+
         <img src={image} alt={""} />
       </div>
+
       <div className="base-popup-modal__body">
         <ProfileInfoWithFollowButton
           name={owner?.name || ""}
-          imageUrl={owner.imageUrl}
-          webId={owner.webId}
+          imageUrl={owner?.imageUrl}
+          webId={owner?.webId}
         />
 
-        {/* {!isUserInSessionTheOwner && ( */}
-        <div className="popup-modal-social-icons">
-          {isSaved ? (
-            <FavoriteIcon
-              data-testid="unsave-point-button"
-              sx={{
-                color: "#ef233c",
-              }}
-              onClick={(e: any) => handleUnSavePoint(e)}
-            />
-          ) : (
-            <FavoriteBorderIcon
-              data-testid="save-point-button"
-              sx={{
-                color: "#ef233c",
-              }}
-              onClick={(e: any) => handleSavePoint(e)}
-            />
-          )}
-        </div>
+        {/* {owner?.webId && owner.webId !== session.info.webId && ( */}
+          <div className="popup-modal-social-icons">
+            {isSaved ? (
+              <FavoriteIcon
+                data-testid="unsave-point-button"
+                sx={{
+                  color: "#ef233c",
+                }}
+                onClick={(e: any) => handleUnSavePoint(e)}
+              />
+            ) : (
+              <FavoriteBorderIcon
+                data-testid="save-point-button"
+                sx={{
+                  color: "#ef233c",
+                }}
+                onClick={(e: any) => handleSavePoint(e)}
+              />
+            )}
+          </div>
         {/* )} */}
       </div>
+
       <div className="base-popup-modal__footer">
         <div className="popup-model-footer__contact-info">
           <p>
             {name?.length > 25 ? name.substring(0, 25).concat("...") : name}
           </p>
+
           <p>
             {location?.address?.length > 30
               ? location?.address.substring(0, 30).concat("...")
               : location?.address}
           </p>
         </div>
+
         <BaseButton
           type="button-blue"
           text="Ver punto"
--- webapp/src/components/points/details/review/single/SingleReview.tsx
+++ webapp/src/components/points/details/review/single/SingleReview.tsx
@@ -1,13 +1,13 @@
-import "../../../../../public/css/components/points/details/review/single/SingleReview.css"
-import { Reviewer } from "../../../../../shared/shareddtypes"
-import NoImageSkeleton from "../../../../skeletons/NoImageSkeleton"
-import BaseStarRating from "../../../../stars/BaseStarRating"
-import {differenceInHours } from 'date-fns'
-import IconButton from "../../../../buttons/IconButton"
-import { deleteReviewByPoint } from "../../../../../api/point.api"
-import { useSession } from "@inrupt/solid-ui-react"
-import { useNavigate } from "react-router-dom"
-import { HOME_PATH } from "../../../../../routes"
+import { useSession } from "@inrupt/solid-ui-react";
+import { useNavigate } from "react-router-dom";
+import { deleteReviewByPoint } from "../../../../../api/point.api";
+import "../../../../../public/css/components/points/details/review/single/SingleReview.css";
+import { HOME_PATH } from "../../../../../routes";
+import { Reviewer } from "../../../../../shared/shareddtypes";
+import { formatDateAsRelative } from "../../../../../utils/dateUtils";
+import IconButton from "../../../../buttons/IconButton";
+import NoImageSkeleton from "../../../../skeletons/NoImageSkeleton";
+import BaseStarRating from "../../../../stars/BaseStarRating";
 
 type Props = {
     comment: string;
@@ -15,67 +15,68 @@ type Props = {
     reviewer: Reviewer;
     createdAt: Date;
     pointId: string;
-    reviewId:string;
-    
-}
+    reviewId: string;
+};
 
-function SingleReview(review:Props){
+function SingleReview(review: Props) {
 
-    const {session} = useSession();
+    const { session } = useSession();
     const navigate = useNavigate();
 
-    const  handleDeleteReview = async (e: React.MouseEvent<HTMLElement>) => {
+    const handleDeleteReview = async (e: React.MouseEvent<HTMLElement>) => {
         e.preventDefault();
 
         //Comprobamos que el que va a borrar la review, sea el creador
+
         const pointId = review.pointId;
         const webIdCreator = review.reviewer.webId;
         const reviewId = review.reviewId;
-        
-        console.log(webIdCreator);
-        console.log(session.info.webId);
-        if(webIdCreator == session.info.webId){
-            (console.log("Llegue aqui"));
+
+        if (webIdCreator == session.info.webId) {
             //La eliminamos
-            await deleteReviewByPoint(pointId,reviewId,webIdCreator);
+            await deleteReviewByPoint(pointId, reviewId, webIdCreator);
             //Reddireccion a home
             navigate(HOME_PATH);
         }
-    }
+    };
 
-    const hoursDiff = differenceInHours(review.createdAt,new Date()) * -1;
+    const relativeDate = formatDateAsRelative(review.createdAt);
 
-    const nombre =review.reviewer.name ? review.reviewer.name : review.reviewer.webId.split("/")[2]
+    const nombre = review.reviewer.name
+        ? review.reviewer.name
+        : review.reviewer.webId.split("/")[2] || "Anónimo";
 
-    return(
+    return (
         <div className="single-review-container" role="container">
             <div className="single-review-top" role="data">
-    
                 <div className="single-review-user" role="user-data">
-                    {
-                        review.reviewer.imageUrl ? 
-                        <img src= {review.reviewer.imageUrl} />
-                        : <NoImageSkeleton isRound={true}/>
-                    }
+                    {review.reviewer.imageUrl ? (
+                        <img src={review.reviewer.imageUrl} />
+                    ) : (
+                        <NoImageSkeleton isRound={true} />
+                    )}
                     <div className="single-review-user-data">
                         <h3>{nombre}</h3>
-                        <p>Hace {hoursDiff} horas</p>
+                        {/* <p>Hace {hoursDiff} horas</p> */}
+                        <p>{relativeDate}</p>
                     </div>
                 </div>
 
                 <div className="single-review-rating">
-                    <BaseStarRating rating ={review.rating}/>
+                    <BaseStarRating rating={review.rating} />
                 </div>
-
             </div>
-            <div className="single-review-comment">
-                {review.comment}
-            </div>
+            <div className="single-review-comment">{review.comment}</div>
             <div className="single-review-delete" role="delete">
-                <IconButton type="button-red-form" text="Eliminar" muaIconName="delete" handleClick={handleDeleteReview} />
+                <IconButton
+                    type="button-red-form"
+                    text="Eliminar"
+                    muaIconName="delete"
+                    handleClick={handleDeleteReview}
+                />
             </div>
         </div>
-    )
+    );
 }
 
-export default SingleReview
+export default SingleReview;
