a84aebedc6520d7799bfe56f027b280e2eaa3444
UO282337 <UO282337@uniovi.es>
2023-04-27 18:07:35 +0200

    Arreglados los test e2e del login

--- webapp/e2e/steps/register-form.steps.ts
+++ webapp/e2e/steps/register-form.steps.ts
@@ -1,4 +1,3 @@
-import { te } from 'date-fns/locale';
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import puppeteer from "puppeteer";
 
@@ -24,65 +23,6 @@ defineFeature(feature, test => {
   });
 
   // eslint-disable-next-line jest/no-done-callback
-  test('The user is not registered in the site', ({given,when,then}) => {
-    
-    let username:string;
-    let password:string;
-
-    given('An unregistered user', () => {
-      username = "username"
-      password = "username"
-    });
-
-    when('I fill the data in the form and press submit', async () => {
-      const combo = await page.$('#provider');
-  
-      // hacemos click en el combobox
-      await combo?.click();
-
-      await wait(200)
-
-      // seleccionamos el proveedor (Solid Community)
-      await expect(page).toClick('option', { text: 'Solid Community' })
-
-      await wait(200)
-
-      // hacemos click en el botón de Log In
-      await expect(page).toClick('button', { text: 'Log In' })
-
-      // en la página de solid: introducimos los datos en el formulario y seleccionamos el botón de login de inrupt para que nos redirija a la web
-
-      // Seleccionar el campo del username
-      const inputUser = await page.$('#username');
-        
-      // Escribir texto en el campo del username
-      await inputUser?.type(username);
-
-      // Seleccionar el campo del password
-      const inputPassword = await page.$('#password');
-        
-      // Escribir texto en el campo del password
-      await inputPassword?.type(password);
-
-      // seleccionamos el botón del login
-      await expect(page).toClick('button', { text: 'Log In' })
-
-      await wait(5000)
-    });
-
-    then('The page of the map is shown in the screen', async () => {
-        const text = await page.evaluate(() => document.body.textContent);
-        expect(text).toMatch("Lomap");
-        expect(text).toMatch("Guardados");
-        expect(text).toMatch("Añadir punto");
-        expect(text).toMatch("Acerca de");
-        expect(text).toMatch("Filtro");
-        expect(text).toMatch("Puntos de interés recientes");
-        expect(text).toMatch("© 2023 Lomap. Todos los Derechos Reservados.")
-      });
-  })
-
-  // eslint-disable-next-line jest/no-done-callback
   test('The user is not logged and introduces wrong data', ({given,when,then}) => {
 
     let username:string;
@@ -94,39 +34,24 @@ defineFeature(feature, test => {
     });
 
     when('I fill wrong data in the form and press submit', async () => {
-      const combo = await page.$('#provider');
-  
-      // hacemos click en el combobox
-      await combo?.click();
-
-      await wait(200)
-
-      // seleccionamos el proveedor (Solid Community)
-      await expect(page).toClick('option', { text: 'Solid Community' })
-
-      await wait(200)
-
       // hacemos click en el botón de Log In
       await expect(page).toClick('button', { text: 'Log In' })
 
+      await wait(4000)
+
       // en la página de solid: introducimos los datos en el formulario y seleccionamos el botón de login de inrupt para que nos redirija a la web
 
-      // Seleccionar el campo del username
-      const inputUser = await page.$('#username');
-        
-      // Escribir texto en el campo del username
-      await inputUser?.type(username);
+      await expect(page).toFillForm('form[class="form-horizontal login-up-form"]', {
+        username: username,
+        password: password,
+      })
 
-      // Seleccionar el campo del password
-      const inputPassword = await page.$('#password');
-        
-      // Escribir texto en el campo del password
-      await inputPassword?.type(password);
+      await wait(1000)
 
       // seleccionamos el botón del login
       await expect(page).toClick('button', { text: 'Log In' })
 
-      await wait(5000)
+      await wait(1000)
     });
 
     then('An error message is shown in the screen', async () => {
@@ -134,7 +59,46 @@ defineFeature(feature, test => {
       expect(text).toMatch("No user found for that username");
     });
   })
-  
+
+  // eslint-disable-next-line jest/no-done-callback
+   test('The user is registered in the site', ({given,when,then}) => {
+    
+    let username:string;
+    let password:string;
+
+    given('A registered user', () => {
+      username = "miguelglez"
+      password = "Qwertyuiop1?"
+    });
+
+    when('I fill the data in the form of login and press submit', async () => {
+      // en la página de solid: introducimos los datos en el formulario y seleccionamos el botón de login de inrupt para que nos redirija a la web
+
+      await expect(page).toFillForm('form[class="form-horizontal login-up-form"]', {
+        username: username,
+        password: password,
+      })
+
+      await wait(1000)
+
+      // seleccionamos el botón del login
+      await expect(page).toClick('button', { text: 'Log In' })
+
+      await wait(4000)
+   });
+
+    then('The page of the map is shown in the screen', async () => {
+      const text = await page.evaluate(() => document.body.textContent);
+      expect(text).toMatch("Lomap");
+      expect(text).toMatch("Guardados");
+      expect(text).toMatch("Añadir punto");
+      expect(text).toMatch("Acerca de");
+      expect(text).toMatch("Filtro");
+      expect(text).toMatch("Puntos de interés recientes");
+      expect(text).toMatch("© 2023 Lomap. Todos los Derechos Reservados.")
+    });
+  })
+
   afterAll(async ()=>{
     browser.close()
   })
