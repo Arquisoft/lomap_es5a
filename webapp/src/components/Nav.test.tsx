f1cf6cbea9fa00a13cf824bcb6f27db284c378c0
Marcos Valín Fernández <UO277414@uniovi.es>
2023-04-30 13:51:41 +0200

    test de BaseNav con el maximo coverage que he logrado

--- webapp/src/components/Nav.test.tsx
+++ webapp/src/components/Nav.test.tsx
@@ -1,92 +1,110 @@
+
+
+
+
+
+
 import BaseNav from "./Nav";
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, render} from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 
 
-test ("Comprobamos que se renderizan todos los elementos de la navBar que no dependen del login", () => {
-    // Renderizamos el componente
-    
-    const {getByTestId, getByText} = render(
-    <Router>
-        <BaseNav />
-    </Router>
-    );
-   
 
-    // Recuperamos todos los elementos que lo componen
-    const appLogo = getByTestId("logo");
-    const menuItems = getByText("Inicio");
-            
-
-    // Assert
-    expect(appLogo).toBeInTheDocument();
-    expect(menuItems).toBeInTheDocument();
-            
-});
-
-
-test ("Comprobamos que se renderizan todos los elementos de la navBar que no dependen del login", () => {
-    
-    const mockedSessionLogIn = {
-        session: {
-            info: {
-                isLoggedIn: true
-            }
+const mockedSessionLogIn = {
+    session: {
+        info: {
+            isLoggedIn: true
         }
     }
-    
-    
-    jest.mock("@inrupt/solid-ui-react", () => ({            
-        // 4- Mock the required hook
-        useSession: () => mockedSessionLogIn
-    }));
-    
-    // Renderizamos el componente
-    
-    const {getByTestId, getByText} = render(
-    <Router>
-        <BaseNav />
-    </Router>
-    );
-   
-
-    // Recuperamos todos los elementos que lo componen
-    const appLogo = getByTestId("logo");
-    const menuItems = getByText("Inicio");
-            
-
-    // Assert
-    expect(appLogo).toBeInTheDocument();
-    expect(menuItems).toBeInTheDocument();
-            
-});
-
-test ("Comprobamos que si no estamos logeados se muestra el boton", () => {
-    const mockedSessionLogOut = {
-        session: {
-            info: {
-                isLoggedIn: false
-            }
-        }
-    }
-    
-    
-    jest.mock("@inrupt/solid-ui-react", () => ({            
-        // 4- Mock the required hook
-        useSession: () => mockedSessionLogOut
-    }));
+}
+jest.mock("@inrupt/solid-ui-react", () => ({            
+    // 4- Mock the required hook
+    useSession: () => mockedSessionLogIn
+}));
 
 
-    // Renderizamos el componente
-    const {getByTestId} = render(
+
+ 
+
+describe("Comp NavBar", () => {
+
+    afterAll(cleanup);
+
+    test ("Comprobamos que se renderizan todos los elementos de la navBar que no dependen del login", () => {
+        
+        
+        // Renderizamos el componente
+        
+        const {getByTestId, getByText} = render(
         <Router>
-            <BaseNav/>
+            <BaseNav />
         </Router>
-    );
+        );
+       
+    
+        // Recuperamos todos los elementos que lo componen
+        const appLogo = getByTestId("logo");
+        const menuItems = getByText("Inicio");
 
-    // Comprobamos que se muestra el BaseAvatar (NoImageSkeleton al no tener imagen)
-    const button = getByTestId("button");
-    expect(button).toBeInTheDocument();
+        
 
+                
+    
+        // Comprobamos que aparecen todos los elementos recuperados
+        expect(appLogo).toBeInTheDocument();
+        expect(menuItems).toBeInTheDocument();
+
+                
+    });
+    
+    
+    
+    
+    test ("Comprobamos que se renderizan todos los elementos de la navBar estando logeados", () => {
+        
+
+        
+        // Renderizamos el componente
+        
+        const {getByTestId} = render(
+        <Router>
+            <BaseNav />
+        </Router>
+        );
+       
+    
+        // Recuperamos todos los elementos que lo componen
+        const avatar = getByTestId("skeleton");
+        expect(avatar).toBeInTheDocument();
+                
+    
+                
+    });
+
+    
+
+    // test ("Comprobamos que si no estamos logeados se muestra el boton", async() => {
+    //     // Deshabilitamos el mock para esta prueba
+    //     await waitFor(() => jest.unmock("@inrupt/solid-ui-react"));
+
+
+    
+    //     // Renderizamos el componente
+    //     const {getByTestId} = render(
+    //         <Router>
+    //             <BaseNav/>
+    //         </Router>
+    //     );
+    
+    //     // Comprobamos que se muestra el BaseAvatar (NoImageSkeleton al no tener imagen)
+    //     const button = getByTestId("button");
+    //     expect(button).toBeInTheDocument();
+    
+    // });
+
+  
 });
+
+
+
