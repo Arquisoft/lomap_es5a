6a4d26eb688ea3cb644bf8b04eee19a5be8227aa
RichardPix12 <richard.marques1234@gmail.com>
2023-05-01 12:20:52 +0200

    test e2e addPoint terminados

--- webapp/e2e/steps/addPoint-form.steps.ts
+++ webapp/e2e/steps/addPoint-form.steps.ts
@@ -40,7 +40,7 @@ defineFeature (feature, test => {
             //Solo la primera vez para ir a la pagina de SOLID
             await expect(page).toClick('button', { text: 'Log In' });
 
-            await wait(10000);
+            await wait(6000);
 
             await expect(page).toFillForm('form[class="form-horizontal login-up-form"]', {
                 //Utilizo la cuenta que creo miguel para los test
@@ -53,7 +53,7 @@ defineFeature (feature, test => {
             // seleccionamos el botón del login
             await expect(page).toClick('button', { text: 'Log In' });
         
-            await wait(10000);
+            await wait(7000);
             //Navego a la pagina de la creacion de punto
             await expect(page).toClick('a', {text: 'Añadir punto'});
 
@@ -61,6 +61,8 @@ defineFeature (feature, test => {
 
             //Clicko el boton publicar
             await expect(page).toClick('button', { text: 'Publicar' });
+
+            await wait(3000);
         });
 
         then('An error message is shown in the screen', async () => {
@@ -70,6 +72,7 @@ defineFeature (feature, test => {
     })
 
 //TEST 2
+    // eslint-disable-next-line jest/no-done-callback
     test('The user is logged and have data' , ({given,when,then}) => {
         let name: string;
         
@@ -78,9 +81,14 @@ defineFeature (feature, test => {
         });
 
         when('i fill the form and i presh publish' , async () =>{
+            await wait(2000);
+
             //Relleno la categoria
-            const select = await page.$('select[name="category"]');
-            await select?.select("2");
+            const select = await page.$('#create-point-category-selector');
+            const valueToSelect = 'cinema';
+            await select?.select(valueToSelect);
+
+            await wait(2000);
 
             const input = await page.$('#nombre');
             //Relleno el nombre
@@ -89,6 +97,8 @@ defineFeature (feature, test => {
             await wait(1000);
             //Clicko el boton publicar
             await expect(page).toClick('button', { text: 'Publicar' });
+
+            await wait(8000);
         });
 
         then('I go to home page', async () => {
