b72cc13e0ca92f2094a0eba7e0c2f63ebca19fea
RichardPix12 <richard.marques1234@gmail.com>
2023-05-01 11:33:29 +0200

    test 1 arreglado, test2 creado

--- webapp/e2e/features/addPoint-form.feature
+++ webapp/e2e/features/addPoint-form.feature
@@ -5,3 +5,8 @@ Scenario: The user is registered and no introduce data
   When I navigate to the page and i press publish
   Then An error message is shown in the screen
 
+  Scenario: The user is registered and no introduce category
+  Given A registered user and no category
+  When I navigate to the page and i press publish
+  Then An error message is shown in the screen
+
--- webapp/e2e/steps/addPoint-form.steps.ts
+++ webapp/e2e/steps/addPoint-form.steps.ts
@@ -17,11 +17,13 @@ defineFeature (feature, test => {
           .goto("http://localhost:3000", {
             waitUntil: "networkidle0",
           })
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
           .catch(() => {});
     
       }     
     );
 
+    // eslint-disable-next-line jest/no-done-callback
     test('The user is registered and no introduce data' , ({given,when,then}) => {
         let name : string;
         let address : string;
@@ -53,16 +55,65 @@ defineFeature (feature, test => {
             // seleccionamos el botón del login
             await expect(page).toClick('button', { text: 'Log In' });
         
-            await wait(15000);
+            await wait(10000);
             //Navego a la pagina de la creacion de punto
-            await expect(page).toClick('button', {text: 'Añadir punto'});
+            await expect(page).toClick('a', {text: 'Añadir punto'});
 
-            //Relleno el formulario
-            await expect(page).toFillForm('form[class="createPoint-form"]', {
-                nombre : name,
-                direccion: address,
-                description: description
+            await wait(2000);
+
+            //Clicko el boton publicar
+            await expect(page).toClick('button', { text: 'Publicar' });
+        });
+
+        then('An error message is shown in the screen', async () => {
+            const text = await page.evaluate(() => document.body.textContent);
+            expect(text).toMatch("El campo nombre del punto es obligatorio");
+          });
+    })
+//TEST 2
+    test('The user is registered and no introduce category' , ({given,when,then}) => {
+        let username: string;
+        let password : string;
+        let name: string;
+
+        given ('A registered user and no category', () => {
+            username = "miguelglez";
+            password = "Qwertyuiop1?";   
+        });
+
+        when('I navigate to the page and i press publish' , async () =>{
+
+
+            //Solo la primera vez para ir a la pagina de SOLID
+            await expect(page).toClick('button', { text: 'Log In' });
+
+            await wait(10000);
+
+            await expect(page).toFillForm('form[class="form-horizontal login-up-form"]', {
+                //Utilizo la cuenta que creo miguel para los test
+                username: username,
+                password: password,
             });
+        
+            await wait(1000);
+        
+            // seleccionamos el botón del login
+            await expect(page).toClick('button', { text: 'Log In' });
+        
+            await wait(10000);
+            //Navego a la pagina de la creacion de punto
+            await expect(page).toClick('a', {text: 'Añadir punto'});
+
+            await wait(2000);
+
+            //Relleno el formData
+            await expect(page).toFillForm('form[class="createPoint-form"]', {
+                //Utilizo la cuenta que creo miguel para los test
+                username: username,
+                password: password,
+                nommre : name
+            });
+
             await wait(1000);
             //Clicko el boton publicar
             await expect(page).toClick('button', { text: 'Publicar' });
@@ -74,6 +125,10 @@ defineFeature (feature, test => {
           });
     })
 
+
+    afterAll(async ()=>{
+        browser.close()
+      })
 
     function wait(time: number) {
         return new Promise(function (res) {
